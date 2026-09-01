e2c042f951064a851f5e846bfe085375c2074e95
deepika8248295253-crypto <deepika8248295253@gmail.com>
2026-09-01 14:31:20 +0530

    Update Products.py

--- Products.py
+++ Products.py
@@ -1,1 +1,36 @@
-
+from database import get_connection
+def get_products():
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT
+            id,
+            name,
+            description,
+            price,
+            image_url
+        FROM products
+        ORDER BY id DESC
+    """)
+    products = cursor.fetchall()
+    connection.close()
+    return products
+def add_product(
+    name,
+    description,
+    price,
+    image_url
+):
+    connection = get_connection()
+    connection.execute("""
+        INSERT INTO products
+        (name, description, price, image_url)
+        VALUES (?, ?, ?, ?)
+    """, (
+        name,
+        description,
+        price,
+        image_url
+    ))
+    connection.commit()
+    connection.close()
