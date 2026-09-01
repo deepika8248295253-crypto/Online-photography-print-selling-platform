319a8cd203bc660e269684ec308d2e8b2c17e622
deepika8248295253-crypto <deepika8248295253@gmail.com>
2026-09-01 15:30:47 +0530

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,1 +1,15 @@
-
+def update_status(
+    order_id,
+    status
+):
+    connection = get_connection()
+    connection.execute("""
+        UPDATE orders
+        SET status=?
+        WHERE id=?
+    """, (
+        status,
+        order_id
+    ))
+    connection.commit()
+    connection.close()
