f5da409ffb29fd5823349e5ddd50dc4615fd8aba
deepika8248295253-crypto <deepika8248295253@gmail.com>
2026-09-01 14:51:22 +0530

    Update Orders.py

--- Orders.py
+++ Orders.py
@@ -1,1 +1,20 @@
-
+from database import get_connection
+def place_order(
+    username,
+    product_name,
+    quantity,
+    total
+):
+    connection = get_connection()
+    connection.execute("""
+        INSERT INTO orders
+        (username, product_name, quantity, total)
+        VALUES (?, ?, ?, ?)
+    """, (
+        username,
+        product_name,
+        quantity,
+        total
+    ))
+    connection.commit()
+    connection.close()
