5e762443bda78b5801e39a562ce4af90064e16d7
deepika8248295253-crypto <deepika8248295253@gmail.com>
2026-09-02 13:57:19 +0530

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -1,1 +1,6 @@
-
+from database import get_connection
+def register(username, password):
+    if username == "" or password == "":
+        return False, "Username and password required"
+    connection = get_connection()
+    try:
